5f278da7ad00a07fdb54a103cc75696dbb672eb1
ZHANG Xurui <zxurui@outlook.com>
2019-06-10 02:42:47 +0800

    实现loss函数

---
+++ crossentropyloss.h
@@ -0,0 +1,29 @@
+#ifndef CROSSENTROPY_LOSS_FUNCTION
+#define CROSSENTROPY_LOSS_FUNCTION
+#include "loss.h"
+
+class CrossEntropyLoss : public LossFunction{
+private:
+    static const std::string DimErrMsg;
+    static const std::string RankErrMsg;
+public:
+    Tensor operator() (const Tensor& ans, const Tensor& target);//输入矩阵维度为ans(bat*dim), target(dim)，输出矩阵维度为dim
+};
+
+const std::string CrossEntropyLoss::DimErrMsg = "Dimension Mismatched.";
+const std::string CrossEntropyLoss::RankErrMsg = "The rank in target out of range.";
+
+Tensor CrossEntropyLoss::operator() (const Tensor& ans, const Tensor& target){
+    if(ans.shape().size() != 2 || target.shape().size() != 1 || ans.shape_size(0) != target.shape_size(0))
+        throw DimErrMsg;
+    Tensor output(target.shape(), 0.0f);
+    for(int i = 0; i < output.shape_size(0); i++){
+        int rank = int(target.elem(i));
+        if(rank < 0 || rank >= ans.shape_size(1))
+            throw RankErrMsg;
+        output.elem(i) = - 1 * log(ans.elem(Shape({i, rank})));
+    }
+    return output;
+}
+
+#endif
---
+++ mseloss.h
@@ -0,0 +1,28 @@
+#ifndef MSE_LOSS_FUNCTION
+#define MSE_LOSS_FUNCTION
+#include "loss.h"
+
+class MSELoss : public LossFunction{
+private:
+    static const std::string DimErrMsg;
+public:
+    Tensor operator() (const Tensor& ans, const Tensor& target);//输入矩阵维度为ans(bat*dim), target(bat*dim)，输出矩阵维度为dim
+};
+
+const std::string MSELoss::DimErrMsg = "Dimension Mismatched.";
+
+Tensor MSELoss::operator() (const Tensor& ans, const Tensor& target){
+    if(ans.shape().size() != 2 || target.shape().size() != 2 || ans.shape_size(0) != target.shape_size(0) || ans.shape_size(1) != target.shape_size(1))
+        throw DimErrMsg;
+    Tensor output(Shape({ans.shape_size(0)}), 0.0f);
+    for(int i = 0; i < output.shape_size(0); i++){
+        for(int j = 0; j < ans.shape_size(1); j++){
+            double a = ans.elem(Shape({i, j})), b = target.elem(Shape({i, j}));
+            output.elem(i) += (a-b) * (a-b);
+        }
+        output.elem(i) /= ans.shape_size(1);
+    }
+    return output;
+}
+
+#endif
