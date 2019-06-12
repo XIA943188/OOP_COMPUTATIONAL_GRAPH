db89009215784999bca8540fb5e2abce32562924
XIA943188 <50079399+XIA943188@users.noreply.github.com>
2019-06-12 20:54:21 +0800

    修复了loss增加的bug，所有优化器均已测试

--- main4.cpp
+++ main4.cpp
@@ -21,7 +21,8 @@ ostream &AnsOut = cout; //答案输出至cout
 //节点loss：类型为MSELoss，target为5*1的Tensor（相当于label）
 //优化任务：Wx+b与target的交叉熵损失
 
-const int iter_num = 10;
+const int iter_num = 400;
+const double infinity = 1e9;
 
 int main() {
 	ComGraph<Tensor> neural_network(ErrOut, PriOut); 
@@ -42,28 +43,58 @@ int main() {
     auto label_y = Tensor(Shape({5, 1}), Elem({0, 1, 0, 0, 0}));
     vector<pair<string, Tensor>> PHList;
     PHList.push_back(make_pair("x", sample_x)); PHList.push_back(make_pair("y", label_y));
+    auto Res = Tensor({Shape({1}), infinity});
     for (int iter = 0; iter < iter_num; iter++) {
-        bool failed = false; Tensor Res;
-        AnsOut << "# iter: " << iter << endl;
-        try {
-            Res = neural_network.Eval("loss", PHList);
+        if (Res.elem(0) > 1e-5) {
+            bool failed = false;
+            AnsOut << "# iter: " << iter << ", ";
+            try {
+                Res = neural_network.Eval("loss", PHList);
+            }
+            catch (string &ErrMsg) {
+                ErrOut << ErrMsg << endl;
+                failed = true;
+            }
+            if (!failed) {
+//            auto W = neural_network.Eval("W", PHList), b = neural_network.Eval("b", PHList);
+//            AnsOut << "W:\n" << W << "b:\n" << b << "loss: " << Res << endl;
+                    AnsOut << "loss: " << Res << endl;
+            }
+
+            try {
+                /*
+                //基础版本的梯度下降 (Gradient Descend)
+                neural_network.GradientDescend("dW", "W", PHList);
+                neural_network.GradientDescend("db", "b", PHList);
+                */
+
+                /*
+                //带有动量的梯度下降 (Momentum Gradient Descend)
+                auto init_W_vel = Tensor(Shape({5, 10}), 0.), init_b_vel = Tensor(Shape({5, 1}), 0.);
+                neural_network.MomGradientDescend("dW", "W", PHList, init_W_vel, 0.4);
+                neural_network.MomGradientDescend("db", "b", PHList, init_b_vel, 0.4);
+                */
+
+                /*
+                //RMSprop（参考keras）
+                auto init_W_sqr = Tensor(Shape({5, 10}), 0.), init_b_sqr = Tensor(Shape({5, 1}), 0.);
+                neural_network.RMSprop("dW", "W", PHList, init_W_sqr, 0.8);
+                neural_network.RMSprop("db", "b", PHList, init_b_sqr, 0.8);
+                */
+
+                /*
+                //Adam（参考keras）
+                neural_network.Adam("dW", "W", PHList, 10);
+                neural_network.Adam("db", "b", PHList, 10);
+                */
+            }
+            catch (string &ErrMsg) {
+                ErrOut << ErrMsg << endl;
+            }
         }
-        catch (string &ErrMsg) {
-            ErrOut << ErrMsg << endl;
-            failed = true;
-        }
-        
-        if (!failed) {
-            auto W = neural_network.Eval("W", PHList), b = neural_network.Eval("b", PHList);
-            AnsOut << "W:\n" << W << "b:\n" << b << "loss: " << Res << endl;
-        }
-        
-        try {
-            neural_network.GradientDescend("dW", "W", PHList);
-            neural_network.GradientDescend("db", "b", PHList);
-        }
-        catch (string &ErrMsg) {
-            ErrOut << ErrMsg << endl;
+        else {
+            cout << "# total iter: " << iter << endl;
+            break;
         }
         
     }
